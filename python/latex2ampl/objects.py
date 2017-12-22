4436ab01ab6b93cde5063f69d73bb31502d5cb25
rafaellc28 <rafaellc25@gmail.com>
2017-12-22 18:25:56 -0200

    Optmize lexer

--- python/latex2ampl/objects.py
+++ python/latex2ampl/objects.py
@@ -3,6 +3,6 @@ import ply.yacc as yacc
 import lexer as lexer_mod
 import parser as parser_mod
 
-lexer = lex.lex(module=lexer_mod, debug=False, optimize=False)
+lexer = lex.lex(module=lexer_mod, debug=False, optimize=True)
 parser = yacc.yacc(module=parser_mod, debug=False, optimize=True)
 parser.defaulted_states = {}
