79540e41b0b700603cf00f015dc1fb6ef45c2fb7
rafaellc28 <rafaellc25@gmail.com>
2017-12-26 09:53:45 -0200

    Improved example loading performance.

--- public/js/examples.js
+++ public/js/examples.js
@@ -90,32 +90,17 @@ var initExample = function(sample) {
 					}
 
 					$("#subjMathInput").val(constr);
-					$("#icon-add-constraint").click();
+					//$("#icon-add-constraint").click();
+					UpdateSubjectiveMath(undefined, false);
 				}
 
 			} else {
 				$("#subjMathInput").val(line);
-				$("#icon-add-constraint").click();
+				//$("#icon-add-constraint").click();
+				UpdateSubjectiveMath(undefined, false);
 			}
 		}
 	}
-
-	/*
-	var obj = "\\displaystyle\\sum\\limits_{i \\in I,j \\in J}C_{i,j} * x_{i,j}";
-	$("#objMathInput").val(obj);
-	$("#objMathInput").blur();
 	
-	// initialize constraints
-	var constr = "\\displaystyle\\sum\\limits_{j \\in J}x_{i,j} \\leq A_{i}, i \\in I";
-	$("#subjMathInput").val(constr);
-	$("#icon-add-constraint").click();
-	
-	constr = "\\displaystyle\\sum\\limits_{i \\in I}x_{i,j} \\geq B_{j}, j \\in J,";
-	$("#subjMathInput").val(constr);
-	$("#icon-add-constraint").click();
-	
-	constr = "x_{i,j} \\in \\mathbb{N}";
-	$("#subjMathInput").val(constr);
-	$("#icon-add-constraint").click();
-	*/
+	UpdateSubjectiveMathOutput(mountConstraintsOutput());
 }
--- public/js/mathjaxdisplay.js
+++ public/js/mathjaxdisplay.js
@@ -99,7 +99,7 @@ var initMathjaxDisplay = function () {
 	//  by the user.  Hide the subjectiveFunction, then typeset, then show it again
 	//  so we don't see a flash as the subjMath is cleared and replaced.
 	//
-	window.UpdateSubjectiveMath = function (event) {
+	window.UpdateSubjectiveMath = function (event, updateOutput = true) {
 		if (event) {
 			event.preventDefault();
 		}
@@ -110,7 +110,7 @@ var initMathjaxDisplay = function () {
 
 		var keyTeX = getConstraintKey();
 		addConstraint(keyTeX, TeX);
-		mountConstraints();
+		mountConstraints(updateOutput);
 
 		$("div.MathJax_Display").css("text-align", "left");
 
@@ -242,11 +242,14 @@ var initMathjaxDisplay = function () {
 	    EDITING = key;
 	}
 
-	window.mountConstraints = function() {
+	window.mountConstraints = function(updateOutput = true) {
 		$("#subjectiveMathInput ol").html(mountConstraintsInput());
 		$("#subjMathInput").val("");
 		
-		UpdateSubjectiveMathOutput(mountConstraintsOutput());
+		if (updateOutput) {
+			UpdateSubjectiveMathOutput(mountConstraintsOutput());
+		}
+		
 		
 	    $("#icon-add-constraint").show();
 	    $("#icon-update-constraint").hide();
